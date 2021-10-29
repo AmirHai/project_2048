15a00ec4339362a9eff2991cbe92eed168c18439
AmirHai <59731951+AmirHai@users.noreply.github.com>
2021-10-29 17:01:02 +0300

    добавилось движение цифр вверх и вниз, в будущем будут улучшения

--- AllClasses/GamingProcess.py
+++ AllClasses/GamingProcess.py
@@ -62,52 +62,66 @@ class GameProcess(QWidget):
 
         # второй цикл нужен для того, чтобы кнопки не оказались в одном месте
         # так я задаю два начальных числа
-        for _ in range(2):
-            while self.all_blocks[self.koords[0]][self.koords[1]] != ' ':
-                self.koords = [random.randint(0, 3) for _ in range(2)]
-            self.all_blocks[self.koords[0]][self.koords[1]] = getNumber()
+        while self.all_blocks[self.koords[0]][self.koords[1]] != ' ':
+            self.koords = [random.randint(0, 3) for _ in range(2)]
+        self.all_blocks[self.koords[0]][self.koords[1]] = getNumber()
 
         self.ShowField()
 
     # это показывание нашего поля со всеми значениями, записанную специально в отдельную функцию
     def ShowField(self):
-        self.all_buttons.clear()
         for i in range(4):
+            if len(self.all_buttons) < 4:
+                self.all_buttons.append([])
             for j in range(4):
-                self.all_buttons.append(QPushButton(self.frame_with_buts))
+                if len(self.all_buttons[i]) < 4:
+                    self.all_buttons[i].append(QPushButton(self.frame_with_buts))
                 self.pointSize = 25
                 self.fontD = self.font()
                 self.fontD.setPointSize(self.pointSize)
-                self.all_buttons[-1].setFont(self.fontD)
-                self.all_buttons[-1].setText(self.all_blocks[i][j])
-                self.all_buttons[-1].resize(125, 125)
-                self.all_buttons[-1].move(135 * i + 10, 135 * j + 10)
+                self.all_buttons[i][j].setFont(self.fontD)
+                self.all_buttons[i][j].setText(self.all_blocks[i][j])
+                self.all_buttons[i][j].resize(125, 125)
+                self.all_buttons[i][j].move(135 * i + 10, 135 * j + 10)
 
                 if self.all_blocks[i][j] == ' ':
-                    self.all_buttons[-1].setStyleSheet(f'''background-color: rgb(200, 200, 200);
+                    self.all_buttons[i][j].setStyleSheet(f'''background-color: rgb(200, 200, 200);
                     border-radius: 15px;
                     ''')
                 else:
-                    self.all_buttons[-1].setStyleSheet(
+                    self.all_buttons[i][j].setStyleSheet(
                         f'''background-color: #{COLORS_IN_BLOCKS[sqrOfTwo(self.all_blocks[i][j]) - 1]};
                                         color: #{COLORS_OF_TEXT[TextColorChoose(self.all_blocks[i][j])]};
                                         border-radius: 15px;
                                         ''')
+        self.show()
         print('rrr')
 
     # пока что тут очень много багов, изза чего программа плохо работает.
     # возможно у меня получится исправить их к след понедельнику
     def keyPressEvent(self, event):
-        if event.nativeVirtualKey() == Qt.Key_A:
-            print('qqq')
-            print(*self.all_blocks)
-            for i in range(4):
-                for j in range(3):
-                    if self.all_blocks[i][j] == ' ':
-                        self.all_blocks[i][j] = self.all_blocks[i][j + 1]
-                        self.all_blocks[i][j + 1] = ' '
-            print(*self.all_blocks)
-            self.ShowField()
+        if event.nativeVirtualKey() == Qt.Key_W:
+            self.aClickedEvent()
+        if event.nativeVirtualKey() == Qt.Key_S:
+            self.sClickedEvent()
+
+    def aClickedEvent(self):
+        print(*self.all_blocks)
+        for i in range(4):
+            for j in range(3, 0, -1):
+                if self.all_blocks[i][j - 1] == ' ':
+                    self.all_blocks[i][j - 1] = self.all_blocks[i][j]
+                    self.all_blocks[i][j] = ' '
+        self.ShowField()
+
+    def sClickedEvent(self):
+        print(*self.all_blocks)
+        for i in range(4):
+            for j in range(3):
+                if self.all_blocks[i][j + 1] == ' ':
+                    self.all_blocks[i][j + 1] = self.all_blocks[i][j]
+                    self.all_blocks[i][j] = ' '
+        self.ShowField()
 
 
 def except_hook(cls, exception, traceback):
