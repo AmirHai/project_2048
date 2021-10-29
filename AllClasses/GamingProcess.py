b982bb5fe02914ca4d031962af94bfc95ccf996a
AmirHai <59731951+AmirHai@users.noreply.github.com>
2021-10-29 18:03:29 +0300

    добавил движение кнопок, их соединение. создал полноценную игру 2048.

--- AllClasses/GamingProcess.py
+++ AllClasses/GamingProcess.py
@@ -12,16 +12,7 @@ COLORS_IN_BLOCKS = ['eee4da', 'ede0c8', 'f2b179', 'f59563',
 COLORS_OF_TEXT = ['776e65', 'f9f6f2']
 
 
-# получение значения числа
-def getNumber():
-    num = random.randint(1, 10)
-    if num == 4:
-        return '4'
-    else:
-        return '2'
-
-
-# нахождение степени двойки
+# нахождение степени двойки для установки цвета
 def sqrOfTwo(two):
     k = 0
     tw = int(two)
@@ -39,10 +30,29 @@ def TextColorChoose(two):
         return 0
 
 
+def AddNewNumber(allNumbs):
+    AllClosed = True
+    Opened = []
+    for i in range(len(allNumbs)):
+        for j in range(len(allNumbs[i])):
+            if allNumbs[i][j] == ' ':
+                AllClosed = False
+                Opened.append((i, j))
+    if not AllClosed:
+        num = random.randint(1, 10)
+        if num == 4:
+            numb = '4'
+        else:
+            numb = '2'
+        koords = random.choice(Opened)
+        allNumbs[koords[0]][koords[1]] = numb
+
+
 class GameProcess(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('../AllActivities/Game.ui', self)
+        self.Points = 0
         self.createWindow()
 
     def createWindow(self):
@@ -60,11 +70,10 @@ class GameProcess(QWidget):
             for j in range(4):
                 self.all_blocks[i].append(' ')
 
-        # второй цикл нужен для того, чтобы кнопки не оказались в одном месте
-        # так я задаю два начальных числа
-        while self.all_blocks[self.koords[0]][self.koords[1]] != ' ':
-            self.koords = [random.randint(0, 3) for _ in range(2)]
-        self.all_blocks[self.koords[0]][self.koords[1]] = getNumber()
+        # при вызове функции у нас появляется новое число, которое автоматически записывается
+        # при этом он создает сразу два числа
+        for _ in range(2):
+            AddNewNumber(self.all_blocks)
 
         self.ShowField()
 
@@ -95,32 +104,123 @@ class GameProcess(QWidget):
                                         border-radius: 15px;
                                         ''')
         self.show()
-        print('rrr')
 
     # пока что тут очень много багов, изза чего программа плохо работает.
     # возможно у меня получится исправить их к след понедельнику
     def keyPressEvent(self, event):
         if event.nativeVirtualKey() == Qt.Key_W:
-            self.aClickedEvent()
+            self.wClickedEvent()
         if event.nativeVirtualKey() == Qt.Key_S:
             self.sClickedEvent()
+        if event.nativeVirtualKey() == Qt.Key_A:
+            self.aClickedEvent()
+        if event.nativeVirtualKey() == Qt.Key_D:
+            self.dClickedEvent()
 
-    def aClickedEvent(self):
-        print(*self.all_blocks)
+    # ВСЕ ФУНКЦИИ ТУТ РАЗНЫЕ И ДЕЛАЮТ РАЗНЫЕ ПРЕОБРАЗОВАНИЯ
+    # даже если тут похоже, что многое одинаково, то нет, преобразовать это не получится в единую систему
+    def wClickedEvent(self):
+        catchDifferents = False
+        for _ in range(4):
+            for i in range(4):
+                for j in range(3, 0, -1):
+                    if self.all_blocks[i][j - 1] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i][j - 1] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
         for i in range(4):
-            for j in range(3, 0, -1):
-                if self.all_blocks[i][j - 1] == ' ':
-                    self.all_blocks[i][j - 1] = self.all_blocks[i][j]
+            for j in range(3):
+                if self.all_blocks[i][j + 1] == self.all_blocks[i][j] and self.all_blocks[i][j] != ' ':
+                    self.all_blocks[i][j + 1] = str(int(self.all_blocks[i][j + 1]) + int(self.all_blocks[i][j]))
                     self.all_blocks[i][j] = ' '
+                    catchDifferents = True
+        for _ in range(4):
+            for i in range(4):
+                for j in range(3, 0, -1):
+                    if self.all_blocks[i][j - 1] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i][j - 1] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
+        if catchDifferents:
+            AddNewNumber(self.all_blocks)
         self.ShowField()
 
     def sClickedEvent(self):
-        print(*self.all_blocks)
+        catchDifferents = False
+        for _ in range(4):
+            for i in range(4):
+                for j in range(3):
+                    if self.all_blocks[i][j + 1] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i][j + 1] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
         for i in range(4):
-            for j in range(3):
-                if self.all_blocks[i][j + 1] == ' ':
-                    self.all_blocks[i][j + 1] = self.all_blocks[i][j]
+            for j in range(3, 0, -1):
+                if self.all_blocks[i][j - 1] == self.all_blocks[i][j] and self.all_blocks[i][j] != ' ':
+                    self.all_blocks[i][j - 1] = str(int(self.all_blocks[i][j - 1]) + int(self.all_blocks[i][j]))
                     self.all_blocks[i][j] = ' '
+                    catchDifferents = True
+        for _ in range(4):
+            for i in range(4):
+                for j in range(3):
+                    if self.all_blocks[i][j + 1] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i][j + 1] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
+        if catchDifferents:
+            AddNewNumber(self.all_blocks)
+        self.ShowField()
+
+    def aClickedEvent(self):
+        catchDifferents = False
+        for _ in range(4):
+            for i in range(3, 0, -1):
+                for j in range(4):
+                    if self.all_blocks[i - 1][j] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i - 1][j] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
+        for i in range(3):
+            for j in range(4):
+                if self.all_blocks[i + 1][j] == self.all_blocks[i][j] and self.all_blocks[i][j] != ' ':
+                    self.all_blocks[i + 1][j] = str(int(self.all_blocks[i + 1][j]) + int(self.all_blocks[i][j]))
+                    self.all_blocks[i][j] = ' '
+                    catchDifferents = True
+        for _ in range(4):
+            for i in range(3, 0, -1):
+                for j in range(4):
+                    if self.all_blocks[i - 1][j] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i - 1][j] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
+        if catchDifferents:
+            AddNewNumber(self.all_blocks)
+        self.ShowField()
+
+    def dClickedEvent(self):
+        catchDifferents = False
+        for _ in range(4):
+            for i in range(3):
+                for j in range(4):
+                    if self.all_blocks[i + 1][j] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i + 1][j] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
+        for i in range(3, 0, -1):
+            for j in range(4):
+                if self.all_blocks[i - 1][j] == self.all_blocks[i][j] and self.all_blocks[i][j] != ' ':
+                    self.all_blocks[i - 1][j] = str(int(self.all_blocks[i - 1][j]) + int(self.all_blocks[i][j]))
+                    self.all_blocks[i][j] = ' '
+                    catchDifferents = True
+        for _ in range(4):
+            for i in range(3):
+                for j in range(4):
+                    if self.all_blocks[i + 1][j] == ' ' and self.all_blocks[i][j] != ' ':
+                        self.all_blocks[i + 1][j] = self.all_blocks[i][j]
+                        self.all_blocks[i][j] = ' '
+                        catchDifferents = True
+        if catchDifferents:
+            AddNewNumber(self.all_blocks)
         self.ShowField()
 
 
