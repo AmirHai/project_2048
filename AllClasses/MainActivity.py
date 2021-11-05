0ae3eb84e8fd5f9f8fcd427ef6bce746c382e2cd
AmirHai <59731951+AmirHai@users.noreply.github.com>
2021-11-05 15:36:01 +0300

    немного изменил пояснительную записку, презентация будет изменена чуть позже, тк я еще не придумал ее стилистику

--- AllClasses/MainActivity.py
+++ AllClasses/MainActivity.py
@@ -1,6 +1,7 @@
 import sys
 
 from registration import *
+from MainMenuClass import *
 from PyQt5.QtWidgets import *
 
 
@@ -8,9 +9,10 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+# окно регистрации еще чутка глючное, так что я его пропустил
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    game = RegistrationClass()
+    game = MainMenuInit()
     game.show()
     sys.excepthook = except_hook
     sys.exit(app.exec_())
